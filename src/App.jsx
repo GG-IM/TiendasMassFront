4eb5c647b8e2e49c3bd891cd98e4801df31cde81
Jim Fernandez <n00316076@upn.pe>
2025-06-23 20:38:57 -0500

    2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import Categorias from './pages/categorias';
 import CheckoutPage from './pages/CheckoutPage';
-import Contacto from './pages/contacto';
+import Contacto from './pages/Contacto';
 import { CarritoProvider } from './context/carContext';
 import { UsuarioProvider } from './context/UserContext';
 import ResultadosBusqueda from './pages/ResultadosBusqueda';
