3e12b63ce3557b17b4ceae53e74e4eef1a9c04d4
Jim Fernandez <n00316076@upn.pe>
2025-06-23 19:29:39 -0500

    BORRADO

--- Frontend/src/pages/LoginPage.jsx
+++
@@ -1,9 +0,0 @@
-// LoginPage.jsx
-import React from 'react';
-import AuthContainer from '../components/auth/AuthContainer';
-
-function LoginPage() {
-  return <AuthContainer />;
-}
-
-export default LoginPage;
